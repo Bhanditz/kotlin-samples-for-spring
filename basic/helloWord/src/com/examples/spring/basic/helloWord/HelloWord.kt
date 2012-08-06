292ff8ff7691aeb4ee73c2dcfeb26ad0a7cab46b
Natalia.Ukhorskaya <natalia.ukhorskaya@jetbrains.com>
2012-08-06 17:21:45 +0400

    Refactoring

--- basic/helloWord/src/com/examples/spring/basic/helloWord/HelloWord.kt
+++ basic/helloWord/src/com/examples/spring/basic/helloWord/HelloWord.kt
@@ -9,10 +9,6 @@ import org.springframework.integration.message.GenericMessage
 * @author Natalia.Ukhorskaya
 */
 
-class HelloWorldApp() {
-
-}
-
 class HelloService() {
     fun sayHello(name: String): String {
         return "Hello, " + name;
@@ -20,10 +16,10 @@ class HelloService() {
 }
 
 fun main(args: Array<String>) {
-    val context = ClassPathXmlApplicationContext("/helloWorld.xml", javaClass<HelloWorldApp>())
-    val inputChannel = context.getBean("inputChannel", javaClass<MessageChannel>()) as? MessageChannel
-    val outputChannel = context.getBean("outputChannel", javaClass<PollableChannel>()) as? PollableChannel
-    inputChannel?.send(GenericMessage<String>("guest"))
-    System.out.println("Output: " + outputChannel?.receive(0)?.getPayload());
+    val context = ClassPathXmlApplicationContext("/helloWorld.xml")
+    val inputChannel = context.getBean("inputChannel", javaClass<MessageChannel>()) as MessageChannel
+    val outputChannel = context.getBean("outputChannel", javaClass<PollableChannel>()) as PollableChannel
+    inputChannel.send(GenericMessage<String>("guest"))
+    println("Output: " + outputChannel.receive(0)?.getPayload());
 }
  
