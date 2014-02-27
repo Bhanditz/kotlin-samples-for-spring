5d135ec4c85edf295689c2680199c4dbfe962412
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2014-02-27 19:38:10 +0400

    Add 'override' to toString (see previous commit)

--- application/cafe/src/com/examples/spring/beans/OrderItem.kt
+++ application/cafe/src/com/examples/spring/beans/OrderItem.kt
@@ -5,10 +5,7 @@ package com.examples.spring.beans
  */
 
 class OrderItem(val orderNumber: Int, val drinkType: DrinkType, val shots: Int, val iced: Boolean) {
-    
-    public fun toString(): String {
-        return "${if (iced) "iced " else "hot "} ${this.shots} shot $drinkType"
+    override fun toString(): String {
+        return "${if (iced) "iced " else "hot "} $shots shot $drinkType"
     }
-
 }
-
